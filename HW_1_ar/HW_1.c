f4610b6cea11447179c135c9b43e795ea1e7cb3e
armzasan <82108985+armzasan@users.noreply.github.com>
2023-06-28 14:48:14 +0700

    Update HW_1.c

--- HW_1_ar/HW_1.c
+++ HW_1_ar/HW_1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-void delete_element(int arr[], int *n, int position) {
+void teerapat(int arr[], int *n, int position) {
     if (position >= *n || position < 0) {
         printf("Position is out of bounds of the array.\n");
         return;
@@ -23,7 +23,7 @@ int main() {
     }
     printf("\n");
 
-    delete_element(arr, &n, 2);  // Delete element at position 2
+    teerapat(arr, &n, 2);  // Delete element at position 2
 
     printf("Array after deletion: ");
     for (int i = 0; i < n; i++) {
